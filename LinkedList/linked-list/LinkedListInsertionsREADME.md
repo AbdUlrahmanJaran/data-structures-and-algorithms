7176c78d2770720bed545436e370ce0584dc5bd9
AbdUlrahmanJaran <abdulrahmanjaran@gmail.com>
2022-03-31 03:52:14 +0300

    Linked List Insertions

---
+++ LinkedList/linked-list/LinkedListInsertionsREADME.md
@@ -0,0 +1,24 @@
+# Challenge Summary
+Write the following methods for the Linked List class:
+1. Append
+2. Insert Before
+3. Insert After
+
+## Whiteboard Process
+<br>
+
+## Approach & Efficiency
+Append method time complixity is O(n)
+Insert Before method time complixity is O(n)
+Insert After method time complixity is O(n)
+
+## Solution
+To Append You should write:
+``list.Append("what you want to Append");``
+To Insert 2 before 1 You should write:
+``list.InsertBefore("1" , "2");``
+To Insert 3 after 2 You should write:
+``list.InsertAfter("2" , "3");``
+
+here is an example:
+![LinkedListInsertionsExample](../images/LinkedListInsertionsExample.png)
--- LinkedList/linked-list/Program.cs
+++ LinkedList/linked-list/Program.cs
@@ -6,12 +6,21 @@ namespace linked_list
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Linked List Insertion");
             LinkedList list = new();
-            list.Insert("1");
-            list.Insert("2");
             list.Insert("3");
-            Console.WriteLine(list.Includes("2"));
+            list.Insert("2");
+            list.Insert("1");
+            Console.WriteLine("This is my list");
+            Console.WriteLine(list.ToString());
+            Console.WriteLine("I want to Append '4'");
+            list.Append("4");
+            Console.WriteLine(list.ToString());
+            Console.WriteLine("I want to Insert '5' After '4'");
+            list.InsertAfter("4", "5");
+            Console.WriteLine(list.ToString());
+            Console.WriteLine("I want to Insert '0' before '1'");
+            list.InsertBefore("1", "0");
             Console.WriteLine(list.ToString());
         }
     }
