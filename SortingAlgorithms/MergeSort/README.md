6523f60bac51df7e88c74fe76197ddc477eb8eb9
AbdUlrahmanJaran <abdulrahmanjaran@gmail.com>
2022-05-28 23:36:48 +0300

    Merge Sort Algo

---
+++ SortingAlgorithms/MergeSort/README.md
@@ -0,0 +1,15 @@
+# Challenge Summary
+Implement Merge Sort Algorithm
+
+## Whiteboard Process
+![Merge Sort Whiteboard](./MergeSort.jpg)
+
+## Approach & Efficiency
+MergeSort function time complixity is O(nLogn)
+
+## Solution
+To use MergeSort function you should have an array and bass it like this:
+
+``SortingAlgorithms.MergeSort(array);``
+
+## Link To [Code](../data-structures-project/SortingAlgorithms.cs) 
--- data-structures-project/SortingAlgorithms.cs
+++ data-structures-project/SortingAlgorithms.cs
@@ -27,5 +27,76 @@ namespace data_structures_project
             }
             return arr;
         }
+
+        public static int [] MergeSort(int[] arr)
+        {
+            if (arr.Length == 0)
+                return null;
+            int n = arr.Length;
+            if (n > 1)
+            {
+                int mid = n / 2;
+                int[] left = new int[mid];
+                int[] right = new int[n - mid];
+
+                for (int i = 0; i < mid; i++)
+                {
+                    left[i] = arr[i];
+                }
+
+                int j = 0;
+                for (int i = mid; i < n; i++)
+                {
+                    right[j] = arr[i];
+                    j++;
+                }
+
+                MergeSort(left);
+                MergeSort(right);
+                Merge(left, right, arr);
+            }
+            return arr;
+        }
+
+        public static void Merge(int [] left, int [] right, int [] arr)
+        {
+            int i = 0;
+            int j = 0;
+            int k = 0;
+
+            while (i < left.Length && j < right.Length)
+            {
+                if (left[i] <= right[j])
+                {
+                    arr[k] = left[i];
+                    i++;
+                }
+                else
+                {
+                    arr[k] = right[j];
+                    j++;
+                }
+                k++;
+            }
+
+            if (i == left.Length)
+            {
+                while (j < right.Length)
+                {
+                    arr[k] = right[j];
+                    j++;
+                    k++;
+                }
+            }
+            else
+            {
+                while (i < left.Length)
+                {
+                    arr[k] = left[i];
+                    i++;
+                    k++;
+                }
+            }
+        }
     }
 }
