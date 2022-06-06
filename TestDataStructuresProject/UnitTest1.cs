acc0a6c5b26ac48cc3164d2055e4897d75cd14d8
AbdUlrahmanJaran <abdulrahmanjaran@gmail.com>
2022-06-07 01:49:45 +0300

    HashTable

--- TestDataStructuresProject/UnitTest1.cs
+++ TestDataStructuresProject/UnitTest1.cs
@@ -1,6 +1,7 @@
 using System;
 using Xunit;
 using data_structures_project;
+using System.Collections.Generic;
 
 namespace TestDataStructuresProject
 {
@@ -295,6 +296,80 @@ namespace TestDataStructuresProject
             int[] arr = { 20, 18, 12, 8, 5, -2 };
             int[] sortedArray = { -2, 5, 8, 12, 18, 20 };
             Assert.Equal(sortedArray, SortingAlgorithms.QuickSort(arr, 0, arr.Length - 1));
+        }
+
+        //HashTableTest
+        [Fact]
+        public void TestHashTableSet()
+        { 
+            HashTable hashTable = new HashTable();
+            hashTable.Set("Luffy", "One Piece");
+
+            Assert.True(hashTable.Contains("Luffy"));
+        }
+
+        [Fact]
+        public void TestHashTableGet()
+        {
+            HashTable hashTable = new HashTable();
+            hashTable.Set("Luffy", "One Piece");
+
+            Assert.Equal("One Piece", hashTable.Get("Luffy"));
+        }
+
+        [Fact]
+        public void TestHashTableNull()
+        {
+            HashTable hashTable = new HashTable();
+
+            Assert.Null(hashTable.Get("Goku"));
+        }
+
+        [Fact]
+        public void TestHashTableList()
+        {
+            HashTable hashTable = new HashTable();
+            hashTable.Set("Luffy", "One Piece");
+            hashTable.Set("Goku", "Dragon Ball");
+            hashTable.Set("Gon", "Hunter X Hunter");
+
+            List<string> list = new List<string>();
+            list.Add("Gon");
+            list.Add("Goku");
+            list.Add("Luffy");
+
+            Assert.Equal(list, hashTable.Keys());
+        }
+
+        [Fact]
+        public void TestHashTableCollision()
+        {
+            HashTable hashTable = new HashTable();
+            hashTable.Set("Luffy", "One Piece");
+            hashTable.Set("yffuL", "Straw Hat");
+
+            Assert.Equal(hashTable.Hash("Luffy"), hashTable.Hash("yffuL"));
+            Assert.True(hashTable.Contains("Luffy"));
+            Assert.True(hashTable.Contains("yffuL"));
+        }
+
+        [Fact]
+        public void TestHashTableCollisionValue()
+        {
+            HashTable hashTable = new HashTable();
+            hashTable.Set("Luffy", "One Piece");
+            hashTable.Set("yffuL", "Straw Hat");
+
+            Assert.Equal("Straw Hat", hashTable.Get("yffuL"));
+        }
+
+        [Fact]
+        public void TestHashTableHashInRange()
+        {
+            HashTable hashTable = new HashTable();
+
+            Assert.True(hashTable.Hash("ABCDEFGHIJKLMNOP") > 0);
+            Assert.True(hashTable.Hash("ABCDEFGHIJKLMNOP") < 1024);
         }
     }
 }
---
+++ data-structures-project/HashTable.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data_structures_project
+{
+    public class HashTable
+    {
+        Bucket[] hashTable = new Bucket[1024];
+        public void Set(string key, string value)
+        {
+            int index = Hash(key);
+            Bucket bucket = new Bucket(key, value);
+
+            if (hashTable[index] == null)
+            {
+                hashTable[index] = bucket;
+            }
+            else
+            {
+                Bucket current = hashTable[index];
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = bucket;
+            }    
+        }
+        public string Get(string key)
+        {
+            int index = Hash(key);
+            Bucket current = hashTable[index];
+            while (current != null)
+            {
+                if (current.key == key)
+                {
+                    return current.value;
+                }
+                current = current.Next;
+            }
+            return null;
+        }
+
+        public bool Contains(string key)
+        {
+            int index = Hash(key);
+            Bucket current = hashTable[index];
+            while (current != null)
+            {
+                if (current.key == key)
+                {
+                    return true;
+                }
+                current = current.Next;
+            }
+            return false;
+        }
+
+        public List<string> Keys()
+        {
+            List<string> keys = new List<string>();
+            for (int i = 0; i < hashTable.Length; i++)
+            {
+                Bucket current = hashTable[i];
+                while (current != null)
+                {
+                    keys.Add(current.key);
+                    current = current.Next;
+                }
+            }
+            return keys;
+        }
+        public int Hash(string key)
+        {
+            int hash = 483;
+            for (int i = 0; i < key.Length; i++)
+            {
+                hash  += key[i];
+            }
+            return hash%1024;
+        }
+    }
+}
